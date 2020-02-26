47e4daa52b8d002f284a7d6b25af6f0345a3e4b7
cosiq <june.namgung0@gmail.com>
2020-02-25 22:31:49 -0800

    7. Stacks and Queues - Brackets

---
+++ Codility/Brackets.py
@@ -0,0 +1,14 @@
+# Write a function that, given a string S consisting of N characters, 
+# returns 1 if S is properly nested and 0 otherwise.
+# For example, given S = "{[()()]}", the function 
+# should return 1 and given S = "([)()]", the function should return 0, as explained above.
+
+def brackets(S):
+	paren = {"{":"}", "[":"]", "(": ")"}
+    queue = []
+    for ch in S:
+        if ch in paren.keys(): queue.append(paren[ch])
+        if ch in paren.values():
+            if len(queue) == 0 or ch != queue[-1]: return 0
+            elif ch == queue[-1]: queue.pop()
+    return int(len(queue) == 0)    

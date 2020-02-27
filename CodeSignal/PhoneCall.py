9ca4c2679239d8d54bae4919afdd074c4e97abe0
cosiq <june.namgung0@gmail.com>
2020-02-26 23:42:36 -0800

    Phone Call

--- CodeSignal/PhoneCall.py
+++ CodeSignal/PhoneCall.py
@@ -3,7 +3,8 @@
 # each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
 # each minute after 10th costs min11 cents.
 # You have s cents on your account before the call. 
-# What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
+# What is the duration of the longest call (in minutes rounded down to the nearest integer) 
+# you can have?
 
 def phoneCall(min1, min2_10, min11, s):
     if s > min1 + 9 * min2_10: return 10 + (s - min1 - min2_10 * 9) // min11

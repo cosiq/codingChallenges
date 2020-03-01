02a921ff63cb4cee1003a392c03f3806cc33946e
cosiq <june.namgung0@gmail.com>
2020-02-29 18:18:59 -0800

    6. Sorting - NumberOfDiscIntersections

--- Codility/NumberOfDiscIntersections.py
+++ Codility/NumberOfDiscIntersections.py
@@ -3,21 +3,6 @@
 # The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
 # Given array A shown above, the function should return 11, as explained above.
 
-# def numbersDisc(A):
-#     events = []
-#     for i, a in enumerate(A):
-#         events += [(i-a, +1), (i+a, -1)]
-#     events.sort(key=lambda x: (x[0], -x[1]))
-#     intersections, active_circles = 0, 0
-#     for _, circle_count_delta in events:
-#         intersections += active_circles * (circle_count_delta > 0)
-#         active_circles += circle_count_delta
-#         if intersections > 10E6: return -1
-#     return intersections
-
-# # Time: O(N)
-# # Space: O(N)
-
 def numbersDisc(A):
     cnt = 0
     for i in range(len(A) - 1):

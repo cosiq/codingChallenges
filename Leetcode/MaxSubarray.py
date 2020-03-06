2e918076c1c30173b6c0da4699b926b507c540bf
cosiq <june.namgung0@gmail.com>
2020-03-05 17:04:36 -0800

    53. Maximum Subarray

--- Leetcode/MaxSubarray.py
+++ Leetcode/MaxSubarray.py
@@ -7,7 +7,6 @@
 #	 Explanation: [4,-1,2,1] has the largest sum = 6.
 
 def maxSubArray(nums: List[int]):
-	
 	localMax, globalMax = nums[0], nums[0]
     for i in range(1, len(nums)):
         localMax = max(nums[i], localMax + nums[i])

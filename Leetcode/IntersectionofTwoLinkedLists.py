f1f34ac9b19174b177788410b5295acb7c81bea2
cosiq <june.namgung0@gmail.com>
2020-03-10 23:50:42 -0700

    160. Intersection of Two Linked Lists

---
+++ Leetcode/IntersectionofTwoLinkedLists.py
@@ -0,0 +1,9 @@
+# Write a program to find the node at which the intersection of two singly linked lists begins.
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def getIntersectionNode(self, headA, headB):
+	

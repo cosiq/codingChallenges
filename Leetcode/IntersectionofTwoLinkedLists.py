24d31760dcf967ced6fe831e233bf0281f6008d8
cosiq <june.namgung0@gmail.com>
2020-03-10 23:54:48 -0700

    160. Intersection of Two Linked Lists

--- Leetcode/IntersectionofTwoLinkedLists.py
+++ Leetcode/IntersectionofTwoLinkedLists.py
@@ -5,5 +5,17 @@ class ListNode:
         self.val = x
         self.next = None
 
-def getIntersectionNode(self, headA, headB):
-	
+	def getIntersectionNode(self, headA, headB):
+		if not (headA and headB): return None
+        currA, currB = headA, headB
+        is_secondA, is_secondB = 0, 0
+        while(currA != currB):
+            if not (is_secondA or currA.next):
+                is_secondA = 1
+                currA = headB
+            else: currA = currA.next
+            if not (is_secondB or currB.next):
+                is_secondB = 1
+                currB = headA
+            else: currB = currB.next
+        return currA
